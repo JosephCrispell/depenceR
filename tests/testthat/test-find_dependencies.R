4653f03fa819f256e83a446ac3f66d6dbdf6ea4d
Joseph Crispell <crispelljoseph@gmail.com>
2023-10-16 19:21:27 +0100

    Added test for finding code lines

--- tests/testthat/test-find_dependencies.R
+++ tests/testthat/test-find_dependencies.R
@@ -1,3 +1,25 @@
 test_that("can find R files", {
   expect_true("test-find_dependencies.R" %in% find_r_files())
 })
+
+test_that("can identify code lines", {
+  file_lines <- c(
+    "# my test file",
+    "# is really great",
+    "here <- 15",
+    "some <- \"code\""
+  )
+  expect_equal(get_code_line_indices(file_lines), c(3, 4))
+
+  file_lines <- c(
+    "This file is a [markdown]() one with lots of `markdown` in it",
+    "and",
+    "some `r print(\"in line R code\")`",
+    "```",
+    "this <- 15",
+    "great <- \"code\"",
+    "```",
+    "code blocks!"
+  )
+  expect_equal(get_code_line_indices(file_lines), c(5, 6))
+})
